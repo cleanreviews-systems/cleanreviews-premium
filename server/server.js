d1731d4a4189bc56a98068b37d925f4f47afc1ef
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 00:43:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// ROUTES
+const reviewRoutes = require("./modules/reviews");
+app.use("/reviews", reviewRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "CleanReviews Premium API is running" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
 
