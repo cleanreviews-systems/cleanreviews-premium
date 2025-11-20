1ff02574de490de7a37336c798f552c31ccf584f
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 18:37:39 +0100

    Update campaigns.controller.js

--- server/controllers/campaigns.controller.js
+++ server/controllers/campaigns.controller.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// Test route pour valider le controller
 router.get("/", (req, res) => {
-    res.json({ message: "Campaigns endpoint OK", campaigns: [] });
+  res.json({ message: "Campaigns endpoint OK" });
 });
 
 module.exports = router;
-
