98e7b3eb41dee200d28139e8bae52d5033261ba0
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 23:02:47 +0100

    Update business.service.js

--- server/services/business.service.js
+++ server/services/business.service.js
@@ -1,17 +1,24 @@
-const db = require('../modules/db');
+// Fake in-memory database
+let businesses = [
+  { id: 1, name: "My First Business", userId: 1 }
+];
 
 function listBusinesses() {
-  const stmt = db.prepare('SELECT * FROM businesses ORDER BY created_at DESC');
-  return stmt.all();
+  return businesses;
 }
 
 function createBusiness(name, userId) {
-  const stmt = db.prepare('INSERT INTO businesses (name, user_id) VALUES (?, ?)');
-  const info = stmt.run(name, userId);
-  return { id: info.lastInsertRowid, name, user_id: userId };
+  const newBusiness = {
+    id: businesses.length + 1,
+    name,
+    userId
+  };
+
+  businesses.push(newBusiness);
+  return newBusiness;
 }
 
 module.exports = {
   listBusinesses,
-  createBusiness,
+  createBusiness
 };
