be77c35f3844cc2cc5734b296edd658dc71ed9c7
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 21:31:52 +0100

    Update campaigns.service.js

--- server/services/campaigns.service.js
+++ server/services/campaigns.service.js
@@ -1,12 +1,28 @@
-const campaignsModule = require("../modules/campaigns.module");
+const db = require('../modules/db');
+
+function listCampaigns() {
+  const stmt = db.prepare('SELECT * FROM campaigns ORDER BY created_at DESC');
+  return stmt.all();
+}
+
+function createCampaign({ userId, businessId, name, type, channel, messageTemplate }) {
+  const stmt = db.prepare(`
+    INSERT INTO campaigns (user_id, business_id, name, type, channel, message_template)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `);
+  const info = stmt.run(userId, businessId || null, name, type, channel, messageTemplate);
+  return {
+    id: info.lastInsertRowid,
+    user_id: userId,
+    business_id: businessId || null,
+    name,
+    type,
+    channel,
+    message_template: messageTemplate,
+  };
+}
 
 module.exports = {
-  createCampaign: async (data) => {
-    return await campaignsModule.createCampaign(data);
-  },
-
-  getCampaigns: async () => {
-    return await campaignsModule.getCampaigns();
-  }
+  listCampaigns,
+  createCampaign,
 };
-
