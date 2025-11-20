a944f2865d77bed94ca9acf10db9237e8ae815ac
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 14:33:54 +0100

    Update business.service.js

--- server/services/business.service.js
+++ server/services/business.service.js
@@ -1,1 +1,12 @@
+const businessModule = require("../modules/business.module");
+
+module.exports = {
+  createBusiness: async (data) => {
+    return await businessModule.createBusiness(data);
+  },
+
+  getBusinesses: async () => {
+    return await businessModule.getBusinesses();
+  }
+};
 
