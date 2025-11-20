96924c54cbeecfb8cf903e5cab956ace46f51e69
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 02:25:52 +0100

    Update campaigns.module.js

--- server/modules/campaigns.module.js
+++ server/modules/campaigns.module.js
@@ -1,1 +1,9 @@
+module.exports = {
+  getCampaigns: () => {
+    return [
+      { id: 1, name: "Google Review Campaign", status: "active" },
+      { id: 2, name: "Email Review Sequence", status: "paused" }
+    ];
+  }
+};
 
