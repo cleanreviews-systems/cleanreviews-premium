d291e8bfff24bed7a0e0dbf1376b52f093eaeb33
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 18:14:16 +0100

    Update business.controller.js

--- server/controllers/business.controller.js
+++ server/controllers/business.controller.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// Exemple simple : renvoie une liste vide au début
+router.get("/", (req, res) => {
+    res.json({ message: "Business endpoint OK", businesses: [] });
+});
+
+// Tu pourras ajouter des routes plus tard, exemple :
+// router.post("/", (req, res) => {})
+
+module.exports = router;
 // business controller
