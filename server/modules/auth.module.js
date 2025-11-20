a4248d00c5d68ee975caf568f9ca6c8acdadfe14
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 17:36:27 +0100

    Update auth.module.js

--- server/modules/auth.module.js
+++ server/modules/auth.module.js
@@ -1,17 +1,1 @@
-const { getUserByEmail, createUser } = require("./db");
-
-module.exports = {
-  registerUser: async (email, password) => {
-    const exists = getUserByEmail(email);
-    if (exists) throw new Error("User already exists");
-
-    return createUser(email, password);
-  },
-
-  loginUser: async (email, password) => {
-    const user = getUserByEmail(email);
-    if (!user || user.password !== password) throw new Error("Invalid credentials");
-    return user;
-  }
-};
 
