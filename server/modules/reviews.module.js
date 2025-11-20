88d0b8c128ba1d555848dd91056ce542c09d333d
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 02:26:15 +0100

    Update reviews.module.js

--- server/modules/reviews.module.js
+++ server/modules/reviews.module.js
@@ -1,1 +1,13 @@
+module.exports = {
+  saveReview: async (data) => {
+    return { success: true, review: data };
+  },
+
+  getReviews: async () => {
+    return [
+      { id: 1, message: "Great service!", rating: 5 },
+      { id: 2, message: "Fast and efficient", rating: 4 }
+    ];
+  }
+};
 
