f6ba1fb9da242719a4128d950b116ec8468b0596
cleanreviews-systems <borgnict@gmail.com>
2025-11-20 14:34:21 +0100

    Update campaigns.service.js

--- server/services/campaigns.service.js
+++ server/services/campaigns.service.js
@@ -1,1 +1,12 @@
+const campaignsModule = require("../modules/campaigns.module");
+
+module.exports = {
+  createCampaign: async (data) => {
+    return await campaignsModule.createCampaign(data);
+  },
+
+  getCampaigns: async () => {
+    return await campaignsModule.getCampaigns();
+  }
+};
 
