35f113414b143707f803cab8a54b529d5277850f
cleanreviews-systems <borgnict@gmail.com>
2025-11-21 16:29:19 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// Route de test
-app.get('/', (req, res) => {
-  res.json({ message: 'CleanReviews Premium API is running' });
+// AUTH
+app.use("/auth", require("./controllers/auth.controller"));
+
+// BUSINESS
+app.use("/business", require("./controllers/business.controller"));
+
+// Ping de test
+app.get("/", (req, res) => {
+  res.json({ message: "CleanReviews Premium API is running" });
 });
-
-// Auth routes
-app.use('/auth', require('./controllers/auth.controller'));
-
-// TODO: plus tard on ajoutera:
-// app.use('/business', require('./controllers/business.controller'));
-// app.use('/reviews', require('./controllers/reviews.controller'));
-// app.use('/campaigns', require('./controllers/campaigns.controller'));
 
 const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
 
-app.listen(PORT, () => {
-  console.log('Server running on port', PORT);
-});
